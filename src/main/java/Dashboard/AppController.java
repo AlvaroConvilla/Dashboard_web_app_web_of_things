848ceff538d9fcbd943eea2a505ff2db0a67fc35
alvaroconvilla <alvaro.contreras.villa@gmail.com>
2018-07-29 19:46:39 +0200

    clean code and add index

--- src/main/java/Dashboard/AppController.java
+++ src/main/java/Dashboard/AppController.java
@@ -17,75 +17,89 @@ public class AppController {
 
     @RequestMapping("/")
     public ModelAndView index() throws ParseException {
-        List<Boolean> states = appService.estados();
+        /*List<Boolean> states = appService.estados();
         List<Boolean> alarms = appService.alarmas();
 
 
         ModelAndView result = new ModelAndView("index").addObject("states", states);
-        result.addObject("alarms", alarms);
+        result.addObject("alarms", alarms);*/
 
+        ModelAndView result = new ModelAndView("index");
+        return result;
+    }
+
+    @RequestMapping("/init")
+    public ModelAndView init() throws ParseException {
+        /*List<Boolean> states = appService.estados();
+        List<Boolean> alarms = appService.alarmas();
+
+
+        ModelAndView result = new ModelAndView("index").addObject("states", states);
+        result.addObject("alarms", alarms);*/
+
+        ModelAndView result = new ModelAndView("init");
         return result;
     }
 
     @RequestMapping("/WeatherStation")
     public ModelAndView weatherStation()throws ParseException{
-        List<Boolean> states = appService.estados();
+/*        List<Boolean> states = appService.estados();
         List<Boolean> alarms = appService.alarmas();
 
 
         ModelAndView result = new ModelAndView("WeatherStation").addObject("states", states);
-        result.addObject("alarms", alarms);
+        result.addObject("alarms", alarms);*/
 
+        ModelAndView result = new ModelAndView("WeatherStation");
         return result;
     }
 
     @RequestMapping("/Dome")
     public ModelAndView dome()throws ParseException{
-        List<Boolean> states = appService.estados();
+/*        List<Boolean> states = appService.estados();
         List<Boolean> alarms = appService.alarmas();
 
-
         ModelAndView result = new ModelAndView("Dome").addObject("states", states);
-        result.addObject("alarms", alarms);
-
+        result.addObject("alarms", alarms);*/
+        ModelAndView result = new ModelAndView("Dome");
         return result;
     }
 
     @RequestMapping("/Mount")
     public ModelAndView mount()throws ParseException{
-        List<Boolean> states = appService.estados();
+/*        List<Boolean> states = appService.estados();
         List<Boolean> alarms = appService.alarmas();
-
 
         ModelAndView result = new ModelAndView("Mount").addObject("states", states);
         //result.addObject("states", states);
-        result.addObject("alarms", alarms);
+        result.addObject("alarms", alarms);*/
 
+        ModelAndView result = new ModelAndView("Mount");
         return result;
     }
 
     @RequestMapping("/Camera")
     public ModelAndView camera()throws ParseException{
-        List<Boolean> states = appService.estados();
+/*        List<Boolean> states = appService.estados();
         List<Boolean> alarms = appService.alarmas();
-
 
         ModelAndView result = new ModelAndView("Camera").addObject("states", states);
         //result.addObject("states", states);
-        result.addObject("alarms", alarms);
+        result.addObject("alarms", alarms);*/
 
+        ModelAndView result = new ModelAndView("Camera");
         return result;
     }
     @RequestMapping("/Fotos")
     public ModelAndView fotos()throws ParseException{
-        List<Boolean> states = appService.estados();
+        /*List<Boolean> states = appService.estados();
         List<Boolean> alarms = appService.alarmas();
-
 
         ModelAndView result = new ModelAndView("Fotos").addObject("states", states);
         //result.addObject("states", states);
-        result.addObject("alarms", alarms);
+        result.addObject("alarms", alarms);*/
 
+        ModelAndView result = new ModelAndView("Fotos");
         return result;
     }
 }
